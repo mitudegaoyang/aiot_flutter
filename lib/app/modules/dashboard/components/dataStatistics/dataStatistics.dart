8d02b1b7fd44c659170b0ce8fe87363ad0d18be2
mitudegaoyang <13683265113@163.com>
2021-03-05 17:45:46 +0800

    feat: dashboard 对接数据

--- lib/app/modules/dashboard/components/dataStatistics/dataStatistics.dart
+++ lib/app/modules/dashboard/components/dataStatistics/dataStatistics.dart
@@ -24,40 +24,68 @@ class DataStatistics extends StatelessWidget {
                 colors: [Color(0xFF555DEC), Color(0xFF6D0BBD)],
               ),
             ),
-            DataCardGrid(
-              title: "用户总数",
-              num: 156,
-              colors: [Color(0xFFFD6E5E), Color(0xFFFE9F67)],
+            Obx(
+              () => DataCardGrid(
+                title: "用户总数",
+                num: c.dataStatistics.value.userTotal != null
+                    ? c.dataStatistics.value.userTotal
+                    : 0,
+                colors: [Color(0xFFFD6E5E), Color(0xFFFE9F67)],
+              ),
             ),
-            DataCardGrid(
-              title: "产品总数",
-              num: 33,
-              colors: [Color(0xFFFF8733), Color(0xFFF7B733)],
+            Obx(
+              () => DataCardGrid(
+                title: "产品总数",
+                num: c.dataStatistics.value.productTotal != null
+                    ? c.dataStatistics.value.productTotal
+                    : 0,
+                colors: [Color(0xFFFF8733), Color(0xFFF7B733)],
+              ),
             ),
-            DataCardGrid(
-              title: "设备总数",
-              num: 3015,
-              colors: [Color(0xFFE49971), Color(0xFFE4B971)],
+            Obx(
+              () => DataCardGrid(
+                title: "设备总数",
+                num: c.dataStatistics.value.deviceTotal != null
+                    ? c.dataStatistics.value.deviceTotal
+                    : 0,
+                colors: [Color(0xFFE49971), Color(0xFFE4B971)],
+              ),
             ),
-            DataCardGrid(
-              title: "应用总数",
-              num: 0,
-              colors: [Color(0xFF50D7B1), Color(0xFF85ED6D)],
+            Obx(
+              () => DataCardGrid(
+                title: "应用总数",
+                num: c.dataStatistics.value.appTotal != null
+                    ? c.dataStatistics.value.appTotal
+                    : 0,
+                colors: [Color(0xFF50D7B1), Color(0xFF85ED6D)],
+              ),
             ),
-            DataCardGrid(
-              title: "规则总数",
-              num: 1,
-              colors: [Color(0xFF53C4EC), Color(0xFF53F4EC)],
+            Obx(
+              () => DataCardGrid(
+                title: "规则总数",
+                num: c.dataStatistics.value.ruleTotal != null
+                    ? c.dataStatistics.value.ruleTotal
+                    : 0,
+                colors: [Color(0xFF53C4EC), Color(0xFF53F4EC)],
+              ),
             ),
-            DataCardGrid(
-              title: "告警总数",
-              num: 1,
-              colors: [Color(0xFFFDAFA7), Color(0xFFFDDFA7)],
+            Obx(
+              () => DataCardGrid(
+                title: "告警总数",
+                num: c.dataStatistics.value.alarmTotal != null
+                    ? c.dataStatistics.value.alarmTotal
+                    : 0,
+                colors: [Color(0xFFFDAFA7), Color(0xFFFDDFA7)],
+              ),
             ),
-            DataCardGrid(
-              title: "工单总数",
-              num: 0,
-              colors: [Color(0xFFC996FF), Color(0xFFC9C6FF)],
+            Obx(
+              () => DataCardGrid(
+                title: "工单总数",
+                num: c.dataStatistics.value.workflowTotal != null
+                    ? c.dataStatistics.value.workflowTotal
+                    : 0,
+                colors: [Color(0xFFC996FF), Color(0xFFC9C6FF)],
+              ),
             ),
           ],
         ),
