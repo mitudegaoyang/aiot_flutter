23eba04fd32638a8b1ffbf837f7c7de60dfa9b4f
mitudegaoyang <13683265113@163.com>
2021-03-05 15:15:22 +0800

    feat: 添加默认路由

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -10,6 +10,7 @@ import 'package:aiot/app/modules/tenant/views/tenant_view.dart';
 part 'app_routes.dart';
 
 class AppPages {
+  // static const INITIAL = Routes.DASHBOARD;
   // static const INITIAL = Routes.TENANT;
   static const INITIAL = Routes.HOME;
 
